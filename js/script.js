1ce001a7e8b4761248893d0ef9c745a9c04ede87
riomaria <bmwsoa@gmail.com>
2021-03-19 05:34:36 +0300

    правки по карточкам каталога и другие мелкие корректировки

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
     const linkModalFeedback = document.querySelector(".contacts__btn");
     const modalFeedback = document.querySelector(".modal-feedback");
     const modalClose = document.querySelector(".modal__close");
-    
+    const modalFocus = document.querySelector("[name=username]");
 
 
     linkModalFeedback.addEventListener("click", function (evt) {
     evt.preventDefault();
     modalFeedback.classList.add("modal--show");
+    modalFocus.focus();
+
     
   })
   modalClose.addEventListener("click", function (evt) {
